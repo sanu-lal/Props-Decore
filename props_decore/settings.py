631d8941fb9bdae6093dfb7bd9254a49d0d013f1
sanu-lal <konnakkalsanu@gmail.com>
2026-04-16 23:53:03 +0530

    debug trye

--- props_decore/settings.py
+++ props_decore/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']  # change later to your domain
 
